2b7716d1427c4b338b88a8c13e312d82f4ca339c
Aakash Solanki <tech2aks@gmail.com>
2022-07-08 19:07:48 +0530

    websocket test

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ic/screen/home.dart';
+import 'package:ic/web_socket.dart';
 
 void main() {
   runApp(
     const ProviderScope(
       child: MaterialApp(
-        home: Home(),
+        home: MyTest(),
+        // home: Home(),
       ),
     ),
   );
